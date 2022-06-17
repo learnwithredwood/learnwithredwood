551150149b0af7168c5a01fc8c79b0551ef96bc8
Amy Dutton <amy@ahhacreative.com>
2022-06-17 12:06:13 +0200

    ✨ AirTable Function up and running

---
+++ api/src/functions/airtable/airtable.ts
@@ -0,0 +1,71 @@
+import type { APIGatewayEvent, Context } from 'aws-lambda'
+import { logger } from 'src/lib/logger'
+import Airtable from 'airtable'
+
+/**
+ * The handler function is your code that processes http request events.
+ * You can use return and throw to send a response or error, respectively.
+ *
+ * Important: When deployed, a custom serverless function is an open API endpoint and
+ * is your responsibility to secure appropriately.
+ *
+ * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
+ * in the RedwoodJS documentation for more information.
+ *
+ * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
+ * @typedef { import('aws-lambda').Context } Context
+ * @param { APIGatewayEvent } event - an object which contains information from the invoker.
+ * @param { Context } context - contains information about the invocation,
+ * function, and execution environment.
+ */
+export const handler = async (event: APIGatewayEvent, context: Context) => {
+  logger.info('Invoked airtable function')
+
+  let statusCode = 200
+  let message = ''
+
+  try {
+    const { email } = event.queryStringParameters
+
+    // make sure the email address is provided
+    if (email === undefined) {
+      statusCode = 400
+      message = `Please specify an email address`
+      throw Error(message)
+    }
+
+    // send the email to airtable
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API }).base(
+      process.env.AIRTABLE_RW_BASE
+    )
+
+    base('Emails').create(
+      { Email: email },
+      { typecast: true },
+      function (err, record) {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(record.getId())
+      }
+    )
+  } catch (error) {
+    return {
+      statusCode,
+      body: {
+        message: error.message,
+      },
+    }
+  }
+
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      data: 'airtable function',
+    }),
+  }
+}
