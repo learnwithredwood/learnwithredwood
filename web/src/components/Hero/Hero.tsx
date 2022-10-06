ad5849c2a4c489a692dbedb8b7374098208f6d2b
Amy Dutton <amy@ahhacreative.com>
2022-10-05 19:32:59 -0500

    💄 Updated hero display

--- web/src/components/Hero/Hero.tsx
+++ web/src/components/Hero/Hero.tsx
@@ -1,28 +1,42 @@
 const Hero = () => {
   return (
-    <div className="pt-20 text-center min-h-44rem mb-20">
-      <a href="/" className="mb-7">
-        <img src="/images/logo.svg" alt="RedwoodJS Logo" className="mx-auto" />
-      </a>
-      <h1 className="text-center text-rangoonGreen mb-10">
-        <div className="font-wide text-5-375rem">LEARN</div>
-        <div className="font-script text-5xl leading-none -mt-5">with</div>
-        <div className="font-condensedAlt text-9xl">REDWOOD</div>
-      </h1>
-      <p className="font-body font-bold text-xl text-center mb-14">
-        Learn Full Stack without the Pain of Full Stack
-      </p>
-      <a className="button" href="#newsletter">
-        Get Started
-      </a>
-      <img
-        className="m-[50px_auto_10px]"
-        src="/images/sponsored-by.svg"
-        alt="Sponsored By ZEAL"
-      />
-      <a href="http://codingzeal.com/" target="_blank" rel="noreferrer">
-        <img className="m-[0_auto]" src="/images/zeal-logo.svg" alt="ZEAL" />
-      </a>
+    <div className="pt-[200px] text-center min-h-44rem mb-20">
+      <div className="relative inline-block">
+        <a
+          href="/"
+          className="absolute -left-[5rem] -top-[8.5rem] -rotate-[16] z-0"
+        >
+          <img
+            src="/images/badge.svg"
+            alt="Learn with Redwood"
+            className="mx-auto"
+          />
+        </a>
+        <h1 className="text-center text-rangoonGreen mb-8 relative z-10">
+          <div className="font-narrow uppercase leading-none text-[6rem]">
+            a comprehensive approach to
+          </div>
+          <div className="font-narrow uppercase leading-none text-[7.5rem]">
+            Full-Stack Development
+          </div>
+        </h1>
+        <p className="font-wide font-bold text-xl text-center mb-14">
+          Learn Full Stack{' '}
+          <span className="text-harleyDavidsonOrange">without the Pain</span> of
+          Full Stack
+        </p>
+        <a className="button-alt" href="#newsletter">
+          Get Started
+        </a>
+        <img
+          className="m-[50px_auto_10px]"
+          src="/images/sponsored-by.svg"
+          alt="Sponsored By ZEAL"
+        />
+        <a href="http://codingzeal.com/" target="_blank" rel="noreferrer">
+          <img className="m-[0_auto]" src="/images/zeal-logo.svg" alt="ZEAL" />
+        </a>
+      </div>
     </div>
   )
 }
