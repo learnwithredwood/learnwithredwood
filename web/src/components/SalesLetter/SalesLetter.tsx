222a5e02cff047e3089500781751c22158949a66
Amy Dutton <amy@ahhacreative.com>
2022-10-06 03:38:02 -0500

    💬 Added content for Sales Letter

--- web/src/components/SalesLetter/SalesLetter.tsx
+++ web/src/components/SalesLetter/SalesLetter.tsx
@@ -1,8 +1,143 @@
 const SalesLetter = () => {
   return (
-    <div>
-      <h2>{'SalesLetter'}</h2>
-      <p>{'Find me in ./web/src/components/SalesLetter/SalesLetter.tsx'}</p>
+    <div className="grid grid-cols-12 gap-x-5">
+      <div className="col-start-4 col-span-6">
+        <p className="text-sinopia font-bold">
+          The skills that enable you to be a front-end developer are very
+          different than the skills you need to be a back-end developer.
+        </p>
+        <p>What got you here, won’t get you there.</p>
+        <p>The logic is different.</p>
+        <p>The approach is different.</p>
+        <p>
+          The end user can’t see the backend, but it’s the part that makes your
+          applications interactive and dynamic. It processes information and
+          allows you to save and manipulate data. The backend transforms a
+          website into a web application.
+        </p>
+      </div>
+
+      <div className="section-heading col-start-3 col-span-8 text-left leading-[6.1rem] mt-10 mb-16">
+        The Backend is what will{' '}
+        <span className="text-tealGreen">
+          take your career to the next level.
+        </span>
+      </div>
+
+      <div className="col-start-4 col-span-6">
+        <p>With that said, let’s be honest, full-stack is hard.</p>
+        <p>
+          <strong className="text-sinopia">
+            It takes time to learn something new.
+          </strong>{' '}
+          — And when it comes to development, it’s one thing to learn it and
+          another to truly know it apply it, and implement it.
+        </p>
+        <p>
+          <strong className="text-sinopia">It takes time.</strong> Time you
+          don’t have.
+        </p>
+        <p>
+          <strong className="text-sinopia">
+            It’s hard to know what to focus on or how it all fits together.
+          </strong>{' '}
+          There are so many moving pieces — APIs, database, data modeling,
+          queries, mutations, resolvers.
+        </p>
+        <p>
+          <strong className="text-sinopia">
+            There are so many libraries and frameworks you could reach for.
+          </strong>{' '}
+          How do you know which is the best? or which ones will stick around?
+          Everything is constantly changing.
+        </p>
+        <p>
+          <strong className="text-sinopia">
+            There’s not a clear path to success.
+          </strong>{' '}
+          Where do you start?
+        </p>
+        <p>
+          <strong className="text-sinopia">Everyone has an opinion.</strong>{' '}
+          What separates the signal from the noise? Where do you start? Who do
+          you trust?
+        </p>
+        <p>You feel stuck. It’s easy to be paralyzed.</p>
+        <p>Instead of trying to make sense of it all. You do nothing.</p>
+        <p>You watch other people succeed, while you stay put.</p>
+        <p>
+          Will I ever get good at this? Will it ever make sense to me? Do I even
+          have what it takes?
+        </p>
+        <h3 className="font-wide uppercase text-2xl text-center mb-10">
+          The answer is YES! YES! and YES!
+        </h3>
+
+        <p>
+          <strong className="text-sinopia">
+            You simply need a strong foundation that’s built to last.
+          </strong>{' '}
+          Let me help you get there. <strong>Make it easy on yourself.</strong>{' '}
+          Don’t take the hard road or the long way.
+        </p>
+      </div>
+
+      <div className="col-start-4 col-span-6 mb-12">
+        <h3 className="font-script text-center text-tealGreen text-5xl mb-2">
+          introducing
+        </h3>
+        <h2 className="section-heading leading-[6.1rem] text-center">
+          Learn with Redwood
+        </h2>
+      </div>
+
+      <div className="col-span-6 col-start-4">
+        <p>
+          Learn with Redwood is a self-paced course that will give you the tools
+          you need and the confidence you deserve to become a full-stack
+          developer.
+        </p>
+        <p>
+          We’ll get you up and running with the best-in-class JavaScript
+          libraries and frameworks.
+        </p>
+      </div>
+
+      <div className="font-wide uppercase text-xl col-start-3 col-span-8 text-center leading-8 mb-11">
+        This course is not about learning redwood
+        <br />
+        <span className="text-sinopia">it’s about learning</span>{' '}
+        <span className="text-tealGreen">with</span>{' '}
+        <span className="text-sinopia">redwood</span>
+      </div>
+
+      <div className="col-span-6 col-start-4 mb-12">
+        <p>
+          Redwood takes the most popular, best-in class libraries and
+          frameworks, and bundles them together in a way that’s approachable.
+        </p>
+        <p>
+          It removes the need for setup and configurations, while still
+          providing flexibility and customizations.
+        </p>
+        <p>
+          Redwood enables you to build stable applications at scale, that are
+          easy to maintain. The included generators, encourage the best
+          practices and workflows, allowing you to hone your craft, through an
+          exceptional developer experience.
+        </p>
+        <p>
+          In this course, you will receive hands-on training, coding an
+          application within a real-world development environment. You’ll leave
+          equipped with everything you need to build a full-stack application.
+        </p>
+      </div>
+
+      <div className="font-wide uppercase text-xl text-center leading-8 mb-11 col-start-2 col-span-10">
+        Our self-paced workshop will give you the
+        <br /> foundation you need to focus on what you do best:{' '}
+        <div className="text-sinopia">build something extraordinary.</div>
+      </div>
     </div>
   )
 }
