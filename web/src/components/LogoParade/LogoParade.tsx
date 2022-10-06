43be280fbb7e2d8c2bf55637aab0c13adba41f17
Amy Dutton <amy@ahhacreative.com>
2022-10-05 19:32:13 -0500

    💄 Styled the Logo Parade

--- web/src/components/LogoParade/LogoParade.tsx
+++ web/src/components/LogoParade/LogoParade.tsx
@@ -1,5 +1,32 @@
+import egghead from './images/egghead.png'
+import freeCodeCamp from './images/free-code-camp.png'
+import logRocket from './images/log-rocket.png'
+import smashingMagazine from './images/smashing-magazine.png'
+
 const LogoParade = () => {
-  return <div className="flex justify-between"></div>
+  return (
+    <div className="flex justify-between bg-starkWhite bg-opacity-40 mix-blend-multiply mx-12 px-20 py-6 mb-24">
+      {/* smashing magazine */}
+      <div className="center mix-blend-multiply">
+        <img src={smashingMagazine} alt="Smashing Magazine" />
+      </div>
+
+      {/* log rocket */}
+      <div className="center mix-blend-multiply">
+        <img src={logRocket} alt="Log Rocket" />
+      </div>
+
+      {/* free code camp */}
+      <div className="center mix-blend-multiply">
+        <img src={freeCodeCamp} alt="Free Code Camp" />
+      </div>
+
+      {/* egghead */}
+      <div className="center mix-blend-multiply">
+        <img src={egghead} alt="Egghead.io" />
+      </div>
+    </div>
+  )
 }
 
 export { LogoParade }
