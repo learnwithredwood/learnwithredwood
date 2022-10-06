476febc81298acb66e77543f9043eec77ffb252a
Amy Dutton <amy@ahhacreative.com>
2022-10-05 19:33:28 -0500

    🚧 WIP: Stubbed out the Sales Letter

---
+++ web/src/components/SalesLetter/SalesLetter.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@redwoodjs/testing/web'
+
+import { SalesLetter } from './SalesLetter'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+
+describe('SalesLetter', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<SalesLetter />)
+    }).not.toThrow()
+  })
+})
---
+++ web/src/components/SalesLetter/SalesLetter.tsx
@@ -0,0 +1,10 @@
+const SalesLetter = () => {
+  return (
+    <div>
+      <h2>{'SalesLetter'}</h2>
+      <p>{'Find me in ./web/src/components/SalesLetter/SalesLetter.tsx'}</p>
+    </div>
+  )
+}
+
+export { SalesLetter }
