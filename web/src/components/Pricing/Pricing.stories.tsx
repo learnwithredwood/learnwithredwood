3710b7bab65718436b91382a0b38c423f1a69dd4
Amy Dutton <amy@ahhacreative.com>
2022-10-05 19:33:13 -0500

    🚧 WIP: Stubbed out the pricing component

---
+++ web/src/components/Pricing/Pricing.stories.tsx
@@ -0,0 +1,7 @@
+import {Pricing} from './Pricing'
+
+export const generated = () => {
+  return <Pricing />
+}
+
+export default { title: 'Components/Pricing' }
---
+++ web/src/components/Pricing/Pricing.tsx
@@ -0,0 +1,10 @@
+const Pricing = () => {
+  return (
+    <div>
+      <h2>{'Pricing'}</h2>
+      <p>{'Find me in ./web/src/components/Pricing/Pricing.tsx'}</p>
+    </div>
+  )
+}
+
+export {Pricing}
